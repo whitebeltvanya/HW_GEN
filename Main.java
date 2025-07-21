d6e1c78f10b56649b343e15d24c9b7bc37f723d0
vanya <whitebeltvanya@gmail.com>
2025-07-21 13:13:07 +0300

    first release

---
+++ Main.java
@@ -0,0 +1,98 @@
+import java.lang.reflect.Array;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DoubleBox<String> box = new DoubleBox<>();
+        System.out.println("111 -> " + box.get(111)); // null
+        System.out.println("SIZE: " + box.size()); // 0
+        System.out.println();
+
+        System.out.println("PUT: " + box.put(111, "First string")); // true
+        System.out.println("111 -> " + box.get(111)); // First string
+        System.out.println("111 -> " + box.get(22)); // null
+        System.out.println("111 -> " + box.get(3333)); // null
+        System.out.println("SIZE: " + box.size()); // 1
+        System.out.println();
+
+        System.out.println("PUT: " + box.put(22, "Second string")); // true
+        System.out.println("111 -> " + box.get(111)); // First string
+        System.out.println("111 -> " + box.get(22)); // Second string
+        System.out.println("111 -> " + box.get(3333)); // null
+        System.out.println("SIZE: " + box.size()); // 2
+        System.out.println();
+
+        System.out.println("PUT: " + box.put(3333, "First string")); // false
+        System.out.println("111 -> " + box.get(111)); // First string
+        System.out.println("111 -> " + box.get(22)); // Second string
+        System.out.println("111 -> " + box.get(3333)); // null
+        System.out.println("SIZE: " + box.size()); // 2
+    }
+}
+
+class RandomBox<T> {
+    private int key;
+    private T value;
+
+    public RandomBox(int key, T value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public T tryUnlock(int key) {
+        if (this.key == key) {
+            return value;
+        } else {
+            return null;
+        }
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
+
+class DoubleBox<T> {
+    private RandomBox<T> first;
+    private RandomBox<T> second;
+    private int size;
+
+    public boolean put(int key, T value) {
+        if (first == null) {
+            first = new RandomBox<>(key, value);
+            size++;
+            return true;
+        }
+
+        if (second == null) {
+            second = new RandomBox<>(key, value);
+            size++;
+            return true;
+        }
+
+        return false;
+    }
+
+    public T get(int key) {
+        T ret = null;
+        if (first != null ) {
+            ret = first.tryUnlock(key);
+        }
+
+        if (ret == null && second != null) {
+            return second.tryUnlock(key);
+        }
+
+        return ret;
+    }
+
+
+    public int size() {
+        return size;
+    }
+}
+
